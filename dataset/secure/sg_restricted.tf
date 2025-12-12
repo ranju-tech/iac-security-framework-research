309920cc43ad465aab3580bd0c721caf484e758b
ranju-tech <ijrdopub.ranju@gmail.com>
2025-12-12 16:58:55 +0530

    Add secure SG dataset sample

---
+++ dataset/secure/sg_restricted.tf
@@ -0,0 +1,10 @@
+resource "aws_security_group" "restricted_ssh" {
+  name = "restricted-ssh"
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["10.0.0.0/16"]
+  }
+}
